078bc4a351b5f8fb05d8deb57292c36d9f7b9c63
KenwoodFox <kenwood364@gmail.com>
2022-02-06 19:57:11 -0500

    still does not work

--- BlueBox/BlueBox.py
+++ BlueBox/BlueBox.py
@@ -4,13 +4,13 @@
 import time
 import coloredlogs, logging
 
-from CameraThread import CameraThread
+from LocalStreamer import LocalStreamer
 
 
 class BlueBox:
     def __init__(self):
         """
-        Stuff that runs when we first start.
+        Construct a BlueBox instance
         """
 
         # Get logger
@@ -19,34 +19,18 @@ class BlueBox:
         # Setup colored logs
         coloredlogs.install(level="DEBUG")
 
-        # Make threads
-        self.thread1 = CameraThread(2)
-        self.thread2 = CameraThread(4)
+        # Each camera should have its own threaded handler.
+        self.CameraThread0 = LocalStreamer(0)
+        self.CameraThread0.daemon = True
+        # self.thread2 = CameraThread(4)
 
     def run(self):
-        self.thread1.start()
-        self.thread2.start()
+        self.CameraThread0.start()
+        # self.thread2.start()
 
-        time.sleep(2.4)
-
-
-## Capture video from webcam
-# vid_capture = cv2.VideoCapture(2)
-# vid_cod = cv2.VideoWriter_fourcc(*"XVID")
-# output = cv2.VideoWriter("videos/cam_video.mp4", vid_cod, 20.0, (640, 480))
-
-# while True:
-#    # Capture each frame of webcam video
-#    ret, frame = vid_capture.read()
-#    cv2.imshow("My cam video", frame)
-#    output.write(frame)
-#    # Close and break the loop after pressing "x" key
-#    if cv2.waitKey(1) & 0xFF == ord("x"):
-#        break
-
-# close the already opened camera
-# vid_capture.release()
-# close the already opened file
-# output.release()
-# close the window and de-allocate any associated memory usage
-# cv2.destroyAllWindows()
+        try:
+            while True:
+                logging.debug("Mainloop is waiting...")
+                time.sleep(1)
+        except KeyboardInterrupt:
+            quit()
--- BlueBox/LocalStreamer.py
+++ BlueBox/LocalStreamer.py
@@ -1,9 +1,10 @@
 import cv2
+import time
 import logging
 import threading
 
 
-class CameraThread(threading.Thread):
+class LocalStreamer(threading.Thread):
     """
     Handles offloading the job of capturing frames
     to a separate thread, construct one for
@@ -28,13 +29,20 @@ class CameraThread(threading.Thread):
     def run(self):
         logging.info("Starting.")
 
-        try:
-            while True:
-                logging.debug(f"Camera number {self.camID} capture a new frame")
-                ret, frame = self.vidCap.read()
+        while True:
+            logging.debug(f"Camera number {self.camID} capture a new frame")
+            ret, frame = self.vidCap.read()
 
+            if not ret:
+                logging.error("Could not capture last frame.")
+
+            else:
                 # Write the frame we just grabbed
                 self.vidWriter.write(frame)
-        except KeyboardInterrupt():
-            self.vidCap.release()
-            self.vidWriter.release()
+                logging.info("Wrote frame.")
+
+            time.sleep(0.01)
+
+    def terminate(self):
+        self.vidCap.release()
+        self.vidWriter.release()
