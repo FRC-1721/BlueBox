d38087264faa03023ec5acf6744427456e54f153
Joe <kenwood364@gmail.com>
2022-02-19 01:06:51 +0000

    Super ugly but almost usable

--- BlueBox/LocalStreamer.py
+++ BlueBox/LocalStreamer.py
@@ -3,6 +3,11 @@ from vidgear.gears.asyncio import WebGear
 from vidgear.gears import CamGear
 from vidgear.gears import VideoGear
 
+# import necessary libs
+import uvicorn, asyncio, cv2
+from vidgear.gears import WriteGear
+from vidgear.gears.asyncio.helper import reducer
+
 import threading
 
 
@@ -23,10 +28,7 @@ class LocalStreamer(threading.Thread):
         # The passed instance of nt from main thread
         self.BlueBoxTable = BlueBoxTable
 
-        # Setup a camgear, to handle capturing this cam
-        # self.Camera = CamGear(source=0, logging=True).start()
-
-        self.options = {
+        self.webGearOptions = {
             "frame_size_reduction": 40,
             "jpeg_compression_quality": 80,
             "jpeg_compression_fastdct": True,
@@ -34,13 +36,66 @@ class LocalStreamer(threading.Thread):
             "custom_data_location": "BlueBox/www/",
         }
 
-        self.web = WebGear(source=0, logging=True, **self.options)
+        writeGearOptions = {
+            "-c:v": "libx264",
+            "-crf": 22,
+            "-map": 0,
+            "-segment_time": 9,
+            "-g": 9,
+            "-sc_threshold": 0,
+            "-force_key_frames": "expr:gte(t,n_forced*9)",
+            "-clones": ["-f", "segment"],
+        }
+
+        # Setup blank WebGear
+        self.web = WebGear(logging=True, **self.webGearOptions)
+
+        self.web.config["generator"] = self.frame_producer
+
+        self.writer = WriteGear(
+            output_filename="/opt/BlueBox/output%03d.mp4",
+            logging=True,
+            **writeGearOptions,
+        )
 
         self.BlueBoxTable.putNumber(f"CamStream{camID}", 1)
+
+    async def frame_producer(self):
+
+        # Setup video stream depending
+        stream = CamGear(
+            source=0,
+            logging=True,
+        ).start()
+
+        while True:
+            # Read frames
+            frame = stream.read()
+
+            # Break if bad or no frame
+            if frame is None:
+                break
+
+            # Write full size frame
+            self.writer.write(frame)
+
+            # Use reducer to compress frame
+            frame = await reducer(frame, percentage=30, interpolation=cv2.INTER_AREA)
+
+            # handle JPEG encoding
+            encodedImage = cv2.imencode(".jpg", frame)[1].tobytes()
+            # yield frame in byte format
+            yield (
+                b"--frame\r\nContent-Type:image/jpeg\r\n\r\n" + encodedImage + b"\r\n"
+            )
+            await asyncio.sleep(0)
+        # safely close video stream
+        stream.stop()
 
     def run(self):
         uvicorn.run(self.web(), host="0.0.0.0", port=8000)
 
     def terminate(self):
+        self.writer.close()
         self.web.shutdown()
         self.Camera.stop()
